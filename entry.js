7cf81f21f546f7c779d7fd9c6d508fc90db5308c
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-27 15:24:43 -0400

    formatted

--- entry.js
+++ entry.js
@@ -1,7 +1,7 @@
-let pop = require('./index.js');
+let pop = require("./index.js");
 let inv = process.argv[2];
 let info = pop(inv);
-console.log('Total Bottles Recycled: ', info.total);
-console.log('Remaining Bottes: ', info.empty);
-console.log('Remaining Bottle Caps: ', info.caps);
-console.log('Bottles Redeemed: ', info.redeem);
+console.log("Total Bottles Recycled: ", info.total);
+console.log("Remaining Bottes: ", info.empty);
+console.log("Remaining Bottle Caps: ", info.caps);
+console.log("Bottles Redeemed: ", info.redeem);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = (function () {
-  return ((inv) => {
+module.exports = (function() {
+  return inv => {
     let full = Math.floor(inv / 2);
     let iter = Math.pow(full, 2) - full + 1;
     let data = {
@@ -7,28 +7,28 @@ module.exports = (function () {
       redeem: 0,
       caps: 0,
       total: full
-    }
-    Array(iter).fill(1).map((e) => {
-      if (data.empty - 2 >= 0) {
-        full += 1;
-        data.redeem += 1;
-        data.empty -= 2;
-      }
-      if (data.caps - 4 >= 0) {
-        full += 1;
-        data.redeem += 1;
-        data.caps -= 4;
-      }
-      while (full > 0) {
-        full--;
-        data.empty += 1
-        data.caps += 1
-      }
-      return e;
-    });
+    };
+    Array(iter)
+      .fill(1)
+      .map(e => {
+        if (data.empty - 2 >= 0) {
+          full += 1;
+          data.redeem += 1;
+          data.empty -= 2;
+        }
+        if (data.caps - 4 >= 0) {
+          full += 1;
+          data.redeem += 1;
+          data.caps -= 4;
+        }
+        while (full > 0) {
+          full--;
+          data.empty += 1;
+          data.caps += 1;
+        }
+        return e;
+      });
     data.total += data.redeem;
     return data;
-  });
+  };
 })();
-
-
