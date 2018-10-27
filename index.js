4f1c569b34215fe3fd9c74b5470435d5a6f52c56
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-27 14:10:31 -0400

    First iteration of algorithm

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 module.exports = (function () {
   return ((inv) => {
-
+    let full = Math.floor(inv / 2);
+    let iter = Math.pow(full, 2) - full + 1;
+    let data = {
+      empty: 0,
+      redeem: 0,
+      caps: 0
+    }
+    Array(iter).fill(1).map((e) => {
+      if (data.empty - 2 >= 0) {
+        full += 1;
+        data.redeem += 1;
+        data.empty -= 2;
+      }
+      if (data.caps - 4 >= 0) {
+        full += 1;
+        data.redeem += 1;
+        data.caps -= 4;
+      }
+      while (full > 0) {
+        full--;
+        data.empty += 1
+        data.caps += 1
+      }
+      console.log(data);
+      return e;
+    });
+    console.log(full);
+    return data;
   });
 })();
+
+
